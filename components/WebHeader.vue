56ddfceac6711405fcefb9c5fdb98ab721febf32
KlausMass <massinemouha@zohomail.com>
2024-06-07 18:54:50 +0100

    Elis improved

--- components/WebHeader.vue
+++ components/WebHeader.vue
@@ -1,7 +1,7 @@
 <template>
     <header class="bg-transparent text-white absolute z-10 w-full">
         <nav class="container mx-auto flex justify-between items-center p-4">
-            <div class="text-xl font-bold">Will Elis</div>
+            <div class="text-xl font-bold">Will Reps</div>
             <ul class="hidden md:flex space-x-4">
                 <li><a href="#home" class="hover:text-orange-500">Home</a></li>
                 <li><a href="#community" class="hover:text-orange-500">Community</a></li>
@@ -9,7 +9,7 @@
                 <li><a href="#services" class="hover:text-orange-500">Services</a></li>
                 <li><a href="#blog" class="hover:text-orange-500">Blog</a></li>
             </ul>
-            <div class="flex cursor-pointer md:hidden flex-col items-center gap-[4px]" @click="toggleSideBar">
+            <div class="flex cursor-pointer md:hidden flex-col items-center gap-[4px]" @click="x => toggleSideBar()">
                 <div v-for="i in 3" class="h-[3px] rounded-full w-6 bg-white"></div>
             </div>
         </nav>
@@ -23,7 +23,7 @@
         <div class="flex justify-between items-center">
             <h3 class="text-xl">Navigation</h3>
 
-            <div class="cursor-pointer relative mr-3" @click="toggleSideBar">
+            <div class="cursor-pointer relative mr-3" @click="x => toggleSideBar()">
                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
                     <path fill="currentColor"
                         d="m12 13.4l-4.9 4.9q-.275.275-.7.275t-.7-.275t-.275-.7t.275-.7l4.9-4.9l-4.9-4.9q-.275-.275-.275-.7t.275-.7t.7-.275t.7.275l4.9 4.9l4.9-4.9q.275-.275.7-.275t.7.275t.275.7t-.275.7L13.4 12l4.9 4.9q.275.275.275.7t-.275.7t-.7.275t-.7-.275z" />
--- pages/index.vue
+++ pages/index.vue
@@ -81,44 +81,120 @@
 
     <!-- Blog Section -->
     <section id="blog" class="container mx-auto py-16">
-        <h2 class="text-3xl md:text-4xl font-bold mb-8 text-center">From the Blog</h2>
+        <h2 class="text-3xl md:text-4xl font-bold mb-8 text-center">Some of My posts</h2>
         <div class="flex flex-wrap -mx-4">
             <!-- Article -->
             <div class="w-full md:w-1/3 px-4 mb-8">
-                <h3 class="text-xl font-bold mb-2">Training Tips</h3>
-                <p>Discover the best practices for staying in top shape.</p>
+                <div class="aspect-square overflow-hidden md:rounded-md bg-cover bg-center bg-[url('/workout.jpg')]">
+                </div>
+                <div class="p-2 md:p-0 mt-2">
+                    <h3 class="text-xl font-bold mb-2">Training Tips</h3>
+                    <p>Discover the best practices for staying in top shape.</p>
+                </div>
             </div>
             <!-- Interview -->
-            <div class="w-full md:w-1/3 px-4 mb-8">
-                <h3 class="text-xl font-bold mb-2">Interviews</h3>
-                <p>Insights from leading athletes and coaches.</p>
-            </div>
+            <a class="w-full md:w-1/3 px-4 mb-8" href="https://www.instagram.com/p/C6_0-ylusMt/">
+                <div class="aspect-square overflow-hidden md:rounded-md bg-cover bg-center bg-[url('/interviews.png')]">
+                </div>
+                <div class="p-2 md:p-0 mt-2">
+                    <h3 class="text-xl font-bold mb-2">Interviews</h3>
+                    <p>Some Interviews of Mine</p>
+                </div>
+            </a>
             <!-- Behind-the-scenes -->
-            <div class="w-full md:w-1/3 px-4 mb-8">
-                <h3 class="text-xl font-bold mb-2">Behind the Scenes</h3>
-                <p>A glimpse into the athlete's training regimen.</p>
-            </div>
+            <a class="w-full md:w-1/3 px-4 mb-8" href="https://www.instagram.com/ellisreps_/">
+                <div class="aspect-square overflow-hidden md:rounded-md bg-cover bg-center bg-[url('/workout.jpg')]">
+                </div>
+                <div class="p-2 md:p-0 mt-2">
+                    <h3 class="text-xl font-bold mb-2">Vlogs</h3>
+                    <p>A glimpse into my training regimen.</p>
+                </div>
+            </a>
         </div>
     </section>
 
     <!-- Store Section -->
     <section id="services" class="bg-gray-100 py-16">
-        <div class="container mx-auto text-center">
-            <h2 class="text-3xl md:text-4xl font-bold mb-4">Our Store</h2>
-            <p class="text-xl md:text-2xl mb-8">Get exclusive merchandise and digital products.</p>
+        <div class="container mx-auto">
+            <div class="">
+                <h2 class="text-3xl md:text-4xl font-bold mb-4 text-center">My Services</h2>
 
-            <div class="flex flex-wrap -mx-4">
-                <!-- Merchandise -->
-                <div class="w-full md:w-1/2 px-4 mb-8">
-                    <h3 class="text-xl font-bold mb-2">Merchandise</h3>
-                    <p>Show your support with branded apparel and gear.</p>
+                <h4 class="text-lg font-bold">
+                    1 on 1 Coaching
+                </h4>
+                <p>
+                    Personal Training 1 on 1 to help you push through your workout sessions and grow faster
+                </p>
+                <h4 class="text-lg font-bold">
+                    Nutritional Guidance
+                </h4>
+                <p>
+                    A meal plan adjusted for your exact needs and goals to aid
+                </p>
+                <h4 class="text-lg font-bold">
+                    Workout Plans
+                </h4>
+                <p>
+                    Custom Workout plans and shedules to work on your weaknesses and push your limits
+                </p>
+            </div>
+
+            <h2 class="text-3xl font-bold mb-4 text-center">Get in Touch!</h2>
+
+            <div class="grid grid-cols-2 -mx-4">
+                <!-- Services -->
+                <div class="w-full px-6 mb-8">
+                    <h2 class="text-xl font-bold mb-4">Social Media!</h2>
+                    <div class="flex flex-col gap-4">
+                        <a href="https://wa.me/" class="flex gap-3 items-center font-bold">
+                            <img src="/LogosWhatsappIcon.svg" />
+                            <span>
+                                +XXX X XXXX XXXX
+                            </span>
+                        </a>
+                        <a href="https://www.instagram.com/ellisreps_/" class="flex gap-3 items-center font-bold">
+                            <img src="/MdiInstagram.svg" />
+                            <span>
+                                @ellisreps_
+                            </span>
+                        </a>
+                    </div>
                 </div>
-                <!-- Digital Products -->
-                <div class="w-full md:w-1/2 px-4 mb-8">
-                    <h3 class="text-xl font-bold mb-2">Digital Products</h3>
-                    <p>Access exclusive content with our e-books, courses, and webinars.</p>
+                <!-- Contact Form -->
+                <div class="px-6 rounded-lg">
+                    <h3 class="text-xl font-bold mb-4">Through Email!</h3>
+                    <form class="space-y-4">
+                        <div>
+                            <label for="name" class="block text-gray-700 font-medium mb-1">
+                                Name
+                            </label>
+                            <input type="text" id="name" placeholder="Enter your name"
+                                class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                                name="name" />
+                        </div>
+                        <div>
+                            <label for="email" class="block text-gray-700 font-medium mb-1">
+                                Email
+                            </label>
+                            <input type="email" id="email" placeholder="Enter your email"
+                                class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                                name="email" />
+                        </div>
+                        <div>
+                            <label for="message" class="block text-gray-700 font-medium mb-1">
+                                Message
+                            </label>
+                            <textarea id="message" name="message" rows="4" placeholder="Enter your message"
+                                class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"></textarea>
+                        </div>
+                        <button type="submit"
+                            class="w-full bg-orange-500 hover:bg-orange-600 text-white font-medium py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
+                            Submit
+                        </button>
+                    </form>
                 </div>
             </div>
+
         </div>
     </section>
 
